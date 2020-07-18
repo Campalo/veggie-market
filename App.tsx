99819600145022558267b29d7771ad3f46df3573
Campalo <25295489+Campalo@users.noreply.github.com>
2020-07-18 18:25:57 +0200

    Add content and design to the product list

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import {useCollection} from "./src/firestore";
 
 export default function App() {
   const products = useCollection("products");
   return (
     <View style={styles.container}>
-      <Text>{products.map(product => product.name).join(", ")}</Text>
+      {products.map(product =>
+      <View style={styles.item}>
+        <Image style={styles.avatar} source={{ uri: product.image}}/>
+        <Text>{product.name}</Text>
+        <Text>{product.price} euros / {product.unit}</Text>
+        <Text>{product.stock} {product.unit} en stock</Text>
+      </View>
+        )}
       <StatusBar style="auto" />
     </View>
   );
@@ -20,4 +27,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    display: "flex",
+    flexDirection:"column",
+    backgroundColor: "lightgrey",
+    borderRadius: 4,
+    padding: 20,
+    margin: 10,
+  },
+  avatar: {
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+  }
 });
