35feb587ec3c607e5febdac9b5ae8390d848df56
Campalo <25295489+Campalo@users.noreply.github.com>
2020-07-18 20:08:04 +0200

    Use FlatList for the product list

--- App.tsx
+++ App.tsx
@@ -1,27 +1,38 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, FlatList, SafeAreaView } from 'react-native';
 import {useCollection, createDoc} from "./src/firestore";
 
 export default function App() {
   const products = useCollection("products");
-  return (
-    <View style={styles.container}>
-      {products.map(product =>
-      <View style={styles.item} key={product.id}>
-        <Image style={styles.avatar} source={{ uri: product.image}}/>
-        <Text>{product.name}</Text>
-        <Text>{product.price} euros / {product.unit}</Text>
-        <Text>{product.stock} {product.unit} in stock</Text>
+
+  const renderProduct = ({ item }: any) => (
+      <View style={styles.item}>
+        <Image style={styles.avatar} source={{ uri: item.image}}/>
+        <Text>{item.name}</Text>
+        <Text>{item.price} euros / {item.unit}</Text>
+        <Text>{item.stock} {item.unit} in stock</Text>
       </View>
-        )}
+  )
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+        data={products}
+        renderItem={renderProduct}
+        keyExtractor={ item => item.id}
+        >
+        </FlatList>
       <Button
-      onPress={ () => createDoc("products")}
-      title = "Add"
-      accessibilityLabel="Add a new product to the list"
+        onPress={ () => createDoc("products")}
+        title = "Add"
+        accessibilityLabel="Add a new product to the list"
       />
-      <StatusBar style="auto" />
-    </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
