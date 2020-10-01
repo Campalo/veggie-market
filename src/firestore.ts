f7499a60d357bb277663a24b0b2fc9e9b565caba
Campalo <25295489+Campalo@users.noreply.github.com>
2020-10-01 11:46:22 +0200

    Display a different profile for sellers

--- src/firestore.ts
+++ src/firestore.ts
@@ -2,7 +2,7 @@ import {firestore, initializeApp} from "firebase";
 import "firebase/firestore";
 import {useState, useEffect} from "react";
 
-type CollectionName = "products" | "users";
+type CollectionName = "products" | "users" | "sellers";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAOkHPIlszN_LLyixr0Ez4UlXneVHQhfBY",
@@ -17,9 +17,17 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 
+// Comment out if you want to connect to the production DB
+if (location.hostname === "localhost") {
+    firestore().settings({
+    host: "localhost:8080",
+    ssl: false
+  });
+}
+
 export const useCollection = (collectionName: CollectionName) => {
     const [values, setValues] = useState<any[]>([]);
-    useEffect( () => {
+    useEffect(() => {
         return firestore().collection(collectionName).onSnapshot((snapshot) => {
             const data = dataCollection(snapshot)
             setValues(data);
@@ -28,8 +36,23 @@ export const useCollection = (collectionName: CollectionName) => {
     return values;
 }
 
+export const useDocument = <T extends object>(collectionName: CollectionName, documentID: string) => {
+    const [value, setValue] = useState<T>({} as any);
+    useEffect(() => {
+        return firestore().collection(collectionName).doc(documentID).onSnapshot((snapshot) => {
+            const data = dataDoc(snapshot)
+            setValue(data as any);
+        })
+    }, [collectionName, documentID])
+    return value;
+}
+
 const dataCollection = (snapshot: firestore.QuerySnapshot) => {
     return snapshot.docs.map(doc => ({...doc.data(), id: doc.id}));
+}
+
+const dataDoc = (snapshot: firestore.DocumentSnapshot) => {
+    return {...snapshot.data(), id: snapshot.id}
 }
 
 
--- src/pages/seller/profile/profile.tsx
+++ src/pages/seller/profile/profile.tsx
@@ -1,36 +1,51 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { Btn} from "../../../common/components/btn";
 import { useToggleMode } from '../../../common/theme/theme';
 import { useButton } from '../../../common/theme/button';
 import { useTranslation } from 'react-i18next';
 import * as firebase from 'firebase';
 import { Separator } from "../../../common/components/separator";
+import { useDocument } from "../../../firestore";
 
-
+interface Seller {
+  id: string,
+  displayName: string,
+  uid: string,
+  photoURL : string,
+  email: string,
+}
 
 const ProfileScreen = () => {
   const toggleMode = useToggleMode();
   const { buttonWithMarginTop } = useButton();
   const { t } = useTranslation();
+  const user = firebase.auth().currentUser;
 
-  const createSeller = () => {
-    const user = firebase.auth().currentUser;
 
-    if (user) {
-      const {uid, email, photoURL, displayName} = user;
-      firebase.firestore().collection("sellers").doc(uid).set({uid, email, photoURL, displayName});
-    }
+  if (user) {
+    const createSeller = () => {
+        const {uid, email, photoURL, displayName} = user;
+        firebase.firestore().collection("sellers").doc(uid).set({uid, email, photoURL, displayName});
+      }
+    const seller = useDocument<Seller>("sellers", user.uid)
+
+    return  (
+      <View style={[buttonWithMarginTop, {flex: 1, alignItems: "stretch"}]}>
+        {!!seller ?
+          <Text>I'm a seller</Text>
+        :
+          <Btn onPress={createSeller}>{t("buyer.profile.becomeSeller")}</Btn>
+        }
+          <Separator />
+          <Btn onPress={toggleMode}>{t("changeTheme")}</Btn>
+      </View>
+     )
+  } else {
+    <Text>Please login or create an account</Text>
   }
 
 
-  return  (
-    <View style={[buttonWithMarginTop, {flex: 1, alignItems: "stretch"}]}>
-        <Btn onPress={createSeller}>{t("buyer.profile.becomeSeller")}</Btn>
-        <Separator />
-        <Btn onPress={toggleMode}>{t("changeTheme")}</Btn>
-    </View>
-   )
 }
 
 export default ProfileScreen;
